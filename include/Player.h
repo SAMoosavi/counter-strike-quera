1f9ebe67f41909c57667edeb8848d15b828eb610
moosavi <moosavi238@gmail.com>
2022-10-19 09:45:34 +0330

    add Player

---
+++ include/Player.h
@@ -0,0 +1,12 @@
+//
+// Created by moosavi on 10/19/22.
+//
+
+#ifndef COUNTER_STRIKE_QUERA_PLAYER_H
+#define COUNTER_STRIKE_QUERA_PLAYER_H
+
+class Player{
+
+};
+
+#endif //COUNTER_STRIKE_QUERA_PLAYER_H
