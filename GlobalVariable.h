71a37e850b11b7a1969ce07f67aab8192ca1dbac
moosavi <moosavi238@gmail.com>
2022-10-19 09:42:21 +0330

    add GlobalVariable.h

---
+++ GlobalVariable.h
@@ -0,0 +1,15 @@
+//
+// Created by moosavi on 10/19/22.
+//
+
+#ifndef COUNTER_STRIKE_QUERA_GLOBALVARIABLE_H
+#define COUNTER_STRIKE_QUERA_GLOBALVARIABLE_H
+
+class GlobalVariable {
+public:
+    static enum type_gun {
+        heavy, pistol
+    };
+};
+
+#endif //COUNTER_STRIKE_QUERA_GLOBALVARIABLE_H
