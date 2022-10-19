487d8cb61a342d946e8e6e085d170dfe803e7530
moosavi <moosavi238@gmail.com>
2022-10-19 10:02:43 +0330

    complete Player.h

--- include/Player.h
+++ include/Player.h
@@ -5,8 +5,23 @@
 #ifndef COUNTER_STRIKE_QUERA_PLAYER_H
 #define COUNTER_STRIKE_QUERA_PLAYER_H
 
-class Player{
+#include <string>
+#include "../Setting.h"
+#include "Gun.h"
 
+using std::string;
+
+class Player {
+public:
+    Player(string name) : NAME(name) {}
+
+private:
+    const string NAME;
+    int health = 100;
+    int money = Setting::START_MONEY;
+    int kills = 0;
+    int killed = 0;
+    Gun *guns[3];
 };
 
 #endif //COUNTER_STRIKE_QUERA_PLAYER_H
