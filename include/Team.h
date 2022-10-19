c65d7bfc53094aa832c96569f2b563f21a333640
moosavi <moosavi238@gmail.com>
2022-10-19 11:07:25 +0330

    edit Team.h

--- include/Team.h
+++ include/Team.h
@@ -5,17 +5,23 @@
 #ifndef COUNTER_STRIKE_QUERA_TEAM_H
 #define COUNTER_STRIKE_QUERA_TEAM_H
 
-#include <vector>
+#include <string>
+#include <map>
 #include "Gun.h"
 #include "Player.h"
 
-using std::vector;
+using std::map;
+using std::string;
 
 class Team {
 public:
+    void add_player(string name);
+
 protected:
-    vector<Player *> players;
-    vector<Gun *> guns;
+    virtual void create_gun() = 0;
+
+    map<string,Player *> players;
+    map<string, Gun *> guns;
     int life = 0;
 };
 
