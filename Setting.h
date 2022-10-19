6129ee609c9770635cecd2ca6fa32c113312137a
moosavi <moosavi238@gmail.com>
2022-10-19 10:02:15 +0330

    add Setting

---
+++ Setting.h
@@ -0,0 +1,15 @@
+//
+// Created by moosavi on 10/19/22.
+//
+
+#ifndef COUNTER_STRIKE_QUERA_SETTING_H
+#define COUNTER_STRIKE_QUERA_SETTING_H
+
+class Setting{
+public:
+    static const int START_MONEY = 1000;
+private:
+    Setting() = default;
+};
+
+#endif //COUNTER_STRIKE_QUERA_SETTING_H
