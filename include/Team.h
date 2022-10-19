bd51ef65c8dfbb59cf4f7cf1d5e3d6dab96b20d4
moosavi <moosavi238@gmail.com>
2022-10-19 10:12:15 +0330

    complete  Team.h

--- include/Team.h
+++ include/Team.h
@@ -5,7 +5,18 @@
 #ifndef COUNTER_STRIKE_QUERA_TEAM_H
 #define COUNTER_STRIKE_QUERA_TEAM_H
 
+#include <vector>
+#include "Gun.h"
+#include "Player.h"
+
+using std::vector;
+
 class Team {
+public:
+protected:
+    vector<Player *> players;
+    vector<Gun *> guns;
+    int life = 0;
 };
 
 #endif //COUNTER_STRIKE_QUERA_TEAM_H
