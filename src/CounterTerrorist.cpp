80a8d32036c3904027b73a1a83237d79bb71bd43
moosavi <moosavi238@gmail.com>
2022-10-19 11:08:42 +0330

    complete CounterTerrorist.create_gun

--- src/CounterTerrorist.cpp
+++ src/CounterTerrorist.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "../include/CounterTerrorist.h"
+
+void CounterTerrorist::create_gun() {
+    this->guns["AK"] = (new Gun("AK", 2700, 31, 100, GlobalVariable::type_gun::heavy));
+    this->guns["AWP"] = (new Gun("AWP", 4300, 110, 50, GlobalVariable::type_gun::heavy));
+
+    this->guns["Revolver"] = (new Gun("Revolver", 600, 51, 150, GlobalVariable::type_gun::pistol));
+    this->guns["Glock-18"] = (new Gun("Glock-18", 300, 11, 200, GlobalVariable::type_gun::pistol));
+
+    this->guns["knife"] = (new Gun("knife", 0, 43, 500, GlobalVariable::type_gun::knife));
+}
