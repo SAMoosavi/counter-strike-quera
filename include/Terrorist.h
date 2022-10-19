70ee5792cc68d438d4ac8204512e380977a486c0
moosavi <moosavi238@gmail.com>
2022-10-19 10:14:53 +0330

    add Terrorist

---
+++ include/Terrorist.h
@@ -0,0 +1,14 @@
+//
+// Created by moosavi on 10/19/22.
+//
+
+#ifndef COUNTER_STRIKE_QUERA_TERRORIST_H
+#define COUNTER_STRIKE_QUERA_TERRORIST_H
+
+#include "Team.h"
+
+class Terrorist:public Team{
+
+};
+
+#endif //COUNTER_STRIKE_QUERA_TERRORIST_H
