bc87dc464703d5a3f5c07604915a85375a31800b
moosavi <moosavi238@gmail.com>
2022-10-19 12:21:35 +0330

    add some function in Player

--- include/Player.h
+++ include/Player.h
@@ -18,6 +18,14 @@ class Player {
 public:
     explicit Player(string name, Gun *knife) : NAME(std::move(name)), KNIFE(knife) {}
 
+    void reset();
+
+    void add_kill() { this->kills++; }
+
+    bool shut(int health);
+
+    void bye_gun(Gun *gun);
+
 private:
     const string NAME;
     int health = 100;
@@ -26,6 +34,8 @@ private:
     int killed = 0;
     vector<Gun *> guns;
     const Gun *KNIFE;
+
+    void can_bye(Gun *gun) const;
 };
 
 #endif //COUNTER_STRIKE_QUERA_PLAYER_H
--- src/Player.cpp
+++ src/Player.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "../include/Player.h"
+
+void Player::reset() {
+    this->health = 100;
+}
+
+bool Player::shut(int health) {
+    this->health -= health;
+    if (this->health <= 0) {
+        this->killed++;
+        this->health = 0;
+        this->guns.clear();
+    }
+    return !this->health;
+}
+
+void Player::bye_gun(Gun *gun) {
+    this->can_bye(gun);
+    this->money -= gun->get_price();
+    this->guns.push_back(gun);
+}
+
+void Player::can_bye(Gun *gun) const {
+    for (auto playerGun: this->guns) {
+        if (gun->get_type() == playerGun->get_type()) {
+            throw "you have a" + (gun->get_type() == GlobalVariable::type_gun::heavy) ? "heavy" : "pistol";
+        }
+    }
+
+    if (gun->get_price() > this->money) {
+        throw "no enough money";
+    }
+}
