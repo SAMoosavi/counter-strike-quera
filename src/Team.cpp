a063c4a2a8b56aa6e248170c74f6facd8ce6fc64
moosavi <moosavi238@gmail.com>
2022-10-19 11:07:42 +0330

    complete Team.add_player

--- src/Team.cpp
+++ src/Team.cpp
@@ -1,4 +1,10 @@
 //
 // Created by moosavi on 10/19/22.
 //
+#include <utility>
+
 #include "../include/Team.h"
+
+void Team::add_player(string name) {
+    this->players[name] = (new Player(std::move(name), this->guns["knife"]));
+}
