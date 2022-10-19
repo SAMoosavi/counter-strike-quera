f52e0430f6ed40c8e4ae54b5956f8c9015e21fae
moosavi <moosavi238@gmail.com>
2022-10-19 09:42:45 +0330

    complete Gun.h

--- include/Gun.h
+++ include/Gun.h
@@ -5,4 +5,23 @@
 #ifndef COUNTER_STRIKE_QUERA_GUN_H
 #define COUNTER_STRIKE_QUERA_GUN_H
 
+#include <string>
+#include "../GlobalVariable.h"
+
+using std::string;
+
+class Gun {
+public:
+    Gun(string name, int price, int health, int money, GlobalVariable::type_gun type) : NAME(name), PRICE(price),
+                                                                                        HEALTH(health), MONEY(money),
+                                                                                        TYPE(type) {}
+
+private:
+    const GlobalVariable::type_gun TYPE;
+    const string NAME;
+    const int PRICE;
+    const int HEALTH;
+    const int MONEY;
+};
+
 #endif //COUNTER_STRIKE_QUERA_GUN_H
