5773ebc378f1f97829484057aa3cb6ba502abad6
moosavi <moosavi238@gmail.com>
2022-10-19 11:08:22 +0330

    edit CounterTerrorist

--- include/CounterTerrorist.h
+++ include/CounterTerrorist.h
@@ -2,7 +2,15 @@
 // Created by moosavi on 10/19/22.
 //
 
-#ifndef COUNTER_STRIKE_QUERA_COUNTERTERRORIST_H
-#define COUNTER_STRIKE_QUERA_COUNTERTERRORIST_H
+#ifndef COUNTER_STRIKE_QUERA_COUNTER_TERRORIST_H
+#define COUNTER_STRIKE_QUERA_COUNTER_TERRORIST_H
 
-#endif //COUNTER_STRIKE_QUERA_COUNTERTERRORIST_H
+#include "Team.h"
+
+class CounterTerrorist : public Team {
+public:
+private:
+    void create_gun() override;
+};
+
+#endif //COUNTER_STRIKE_QUERA_COUNTER_TERRORIST_H
