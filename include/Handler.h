14d756a28c3b91113ed2be84344ef98967672c11
moosavi <moosavi238@gmail.com>
2022-10-20 19:00:51 +0330

    add Handler class

---
+++ include/Handler.h
@@ -0,0 +1,18 @@
+//
+// Created by moosavi on 10/20/22.
+//
+
+#ifndef COUNTER_STRIKE_QUERA_HANDLER_H
+#define COUNTER_STRIKE_QUERA_HANDLER_H
+
+#include <string>
+#include "Time.h"
+
+using std::string;
+
+class Handler {
+};
+
+#include "../src/Handler.h"
+
+#endif //COUNTER_STRIKE_QUERA_HANDLER_H
