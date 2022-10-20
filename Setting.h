acb879896e40fb231e7735fc80bf027eee8afeac
moosavi <moosavi238@gmail.com>
2022-10-20 12:51:01 +0330

    add START_GUN

--- Setting.h
+++ Setting.h
@@ -6,6 +6,8 @@
 #define COUNTER_STRIKE_QUERA_SETTING_H
 
 #include "include/Time.h"
+#include "include/Gun.h"
+#include "include/Guns.h"
 
 class Setting {
 public:
@@ -15,11 +17,13 @@ public:
     static const int LOSE_MONEY = 2400;
     static const Time START_TIME;
     static const Time END_TIME;
+    static const Gun *START_GUN;
 private:
     Setting() = default;
 };
 
 const Time Setting::START_TIME = Time("00:00:000");
 const Time Setting::END_TIME = Time("02:15:000");
+const Gun *Setting::START_GUN = Guns::get_gun("knife", GlobalVariable::access_level::setting);
 
 #endif //COUNTER_STRIKE_QUERA_SETTING_H
