d7bfe93d78018b573e39bffbcea9973e1bb8aeb5
moosavi <moosavi238@gmail.com>
2022-10-20 19:38:36 +0330

    add Logger.h

---
+++ include/Logger.h
@@ -0,0 +1,17 @@
+//
+// Created by moosavi on 10/20/22.
+//
+
+#ifndef COUNTER_STRIKE_QUERA_LOGGER_H
+#define COUNTER_STRIKE_QUERA_LOGGER_H
+
+#include <string>
+#include <iostream>
+using std::string;
+
+class Logger {
+public:
+    static void log_successes(const string &message);
+};
+
+#endif //COUNTER_STRIKE_QUERA_LOGGER_H
---
+++ src/Logger.h
@@ -0,0 +1,12 @@
+//
+// Created by moosavi on 10/20/22.
+//
+
+#include "../include/Logger.h"
+
+using std::cout;
+using std::endl;
+
+void Logger::log_successes(const std::string &message) {
+    cout << message << endl;
+}
