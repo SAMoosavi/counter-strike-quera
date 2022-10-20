8752bf02826e4cb69148d7f31761123e2eee77ea
moosavi <moosavi238@gmail.com>
2022-10-20 19:27:22 +0330

    use Setting.MAX_MEMBER_TEAM in Team.add_player

--- src/Team.h
+++ src/Team.h
@@ -4,7 +4,7 @@
 #include "../include/Team.h"
 
 void Team::add_player(const string &name, const Time &time) {
-    if (this->players.size() == 10)
+    if (this->players.size() == Setting::get_max_member_team())
         throw "this team is full";
     this->players[name] = new Player(name, time);
 }
