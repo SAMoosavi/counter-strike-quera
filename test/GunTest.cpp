95f3618f614ec5c15c1d47ce71df67099375ea54
moosavi <moosavi238@gmail.com>
2022-10-20 12:13:07 +0330

    add GunTest.GetName

--- test/GunTest.cpp
+++ test/GunTest.cpp
@@ -18,22 +18,26 @@ protected:
     Gun *gun{};
 };
 
-TEST_F(GunTest,GetPrice){
-    EXPECT_THAT(this->gun->get_price(),Eq(2700));
+TEST_F(GunTest, GetPrice) {
+    EXPECT_THAT(this->gun->get_price(), Eq(2700));
 }
 
-TEST_F(GunTest,GetHealth){
-    EXPECT_THAT(this->gun->get_health(),Eq(31));
+TEST_F(GunTest, GetHealth) {
+    EXPECT_THAT(this->gun->get_health(), Eq(31));
 }
 
-TEST_F(GunTest,GetMoney){
-    EXPECT_THAT(this->gun->get_money(),Eq(100));
+TEST_F(GunTest, GetMoney) {
+    EXPECT_THAT(this->gun->get_money(), Eq(100));
 }
 
-TEST_F(GunTest,GetType){
-    EXPECT_THAT(this->gun->get_type(),Eq(GlobalVariable::type_gun::heavy));
+TEST_F(GunTest, GetType) {
+    EXPECT_THAT(this->gun->get_type(), Eq(GlobalVariable::type_gun::heavy));
 }
 
-TEST_F(GunTest, GetAccessLevel){
-    EXPECT_THAT(this->gun->get_access_level(),Eq(GlobalVariable::access_level::terrorist));
+TEST_F(GunTest, GetAccessLevel) {
+    EXPECT_THAT(this->gun->get_access_level(), Eq(GlobalVariable::access_level::terrorist));
+}
+
+TEST_F(GunTest, GetName) {
+    EXPECT_THAT(this->gun->get_name(), Eq("AK"));
 }
