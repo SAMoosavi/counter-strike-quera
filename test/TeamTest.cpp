f392fb05764e40723e747b05ee4c712a8616d1ed
moosavi <moosavi238@gmail.com>
2022-10-21 15:28:20 +0330

    complete TeamTest.cpp

--- test/TeamTest.cpp
+++ test/TeamTest.cpp
@@ -1,3 +1,79 @@
 //
 // Created by moosavi on 10/21/22.
 //
+
+#include "../include/Team.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using std::to_string;
+using std::string;
+using testing::IsTrue;
+using testing::IsFalse;
+using testing::NotNull;
+using testing::Eq;
+
+class TeamTest : public testing::Test {
+protected:
+    void SetUp() override {
+        this->team = new Team(GlobalVariable::access_level::terrorist);
+    }
+
+    void TearDown() override {
+        delete this->team;
+    }
+
+    Team *team{};
+};
+
+
+TEST_F(TeamTest, AddPlayer) {
+    ASSERT_NO_THROW(this->team->add_player("0", Time("00:01:000")));
+    int muxMemberTeam = Setting::get_max_member_team();
+    for (int i = 1; i < muxMemberTeam - 1; ++i)
+        ASSERT_NO_THROW(this->team->add_player(to_string(i), Time("00:00:001") * i));
+    ASSERT_ANY_THROW(this->team->add_player(to_string(muxMemberTeam), Time("00:00:001") * muxMemberTeam));
+}
+
+TEST_F(TeamTest, HasPlayer) {
+    int muxMemberTeam = Setting::get_max_member_team();
+    for (int i = 0; i < muxMemberTeam - 2; ++i)
+        this->team->add_player(to_string(i), Time("00:00:001") * i);
+    ASSERT_THAT(this->team->has_player("0"), IsTrue());
+    ASSERT_THAT(this->team->has_player("-1"), IsFalse());
+}
+
+TEST_F(TeamTest, GetPlayer) {
+    this->team->add_player("0", Time("00:00:001"));
+    ASSERT_THAT(this->team->get_player("0"), NotNull());
+    ASSERT_ANY_THROW(this->team->get_player("-1"));
+}
+
+TEST_F(TeamTest, HasLive) {
+    this->team->add_player("0", Time("00:00:001"));
+    ASSERT_THAT(this->team->has_live(), IsTrue());
+    this->team->get_player("0")->shut(100);
+    ASSERT_THAT(this->team->has_live(), IsFalse());
+    this->team->add_player("1", Time(Setting::get_time_add_player()) + Time("00:00:001"));
+    ASSERT_THAT(this->team->has_live(), IsFalse());
+}
+
+TEST_F(TeamTest, Won) {
+    this->team->add_player("0", Time("00:00:001"));
+    this->team->won();
+    EXPECT_THAT(this->team->get_player("0")->get_money(), Eq(Setting::get_start_money() + Setting::get_won_money()));
+}
+
+TEST_F(TeamTest, Lose) {
+    this->team->add_player("0", Time("00:00:001"));
+    this->team->lose();
+    EXPECT_THAT(this->team->get_player("0")->get_money(), Eq(Setting::get_start_money() + Setting::get_lose_money()));
+}
+
+TEST_F(TeamTest, GetLiveNum) {
+    ASSERT_THAT(this->team->get_live_num(), Eq(0));
+    this->team->add_player("0", Time("00:00:001"));
+    ASSERT_THAT(this->team->get_live_num(), Eq(1));
+    this->team->add_player("1", Time(Setting::get_time_add_player()) + Time("00:00:001"));
+    ASSERT_THAT(this->team->get_live_num(), Eq(1));
+}
