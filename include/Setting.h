7e7e87945bd32c294376e118c7c3caf3beb0c93b
moosavi <moosavi238@gmail.com>
2022-10-22 09:44:20 +0330

    move Setting.h to include src

--- include/Setting.h
+++ include/Setting.h
@@ -6,33 +6,31 @@
 #define COUNTER_STRIKE_QUERA_SETTING_H
 
 
-#include "include/Gun.h"
-#include "include/Guns.h"
+#include "Gun.h"
+#include "Guns.h"
 #include <string>
 
 using std::string;
 
 class Setting {
 public:
-    static auto get_start_money() { return Setting::START_MONEY; }
+    static auto get_start_money();
 
-    static auto get_max_money() { return Setting::MAX_MONEY; }
+    static auto get_max_money();
 
-    static auto get_won_money() { return Setting::WON_MONEY; }
+    static auto get_won_money();
 
-    static auto get_lose_money() { return Setting::LOSE_MONEY; }
+    static auto get_lose_money();
 
-    static auto get_start_time() { return Setting::START_TIME; }
+    static auto get_end_time();
 
-    static auto get_end_time() { return Setting::END_TIME; }
+    static auto get_time_buy_gun();
 
-    static auto get_time_buy_gun() { return Setting::TIME_BUY_GUN; }
+    static auto get_time_add_player();
 
-    static auto get_time_add_player() { return Setting::TIME_ADD_PLAYER; }
+    static auto get_start_gun();
 
-    static auto get_start_gun() { return Setting::START_GUN; }
-
-    static auto get_max_member_team() { return Setting::MAX_MEMBER_TEAM; }
+    static auto get_max_member_team();
 
 private:
     static const int START_MONEY = 1000;
@@ -40,7 +38,6 @@ private:
     static const int WON_MONEY = 2700;
     static const int LOSE_MONEY = 2400;
     static const int MAX_MEMBER_TEAM = 10;
-    static const string START_TIME;
     static const string END_TIME;
     static const string TIME_BUY_GUN;
     static const string TIME_ADD_PLAYER;
@@ -49,11 +46,12 @@ private:
     Setting() = default;
 };
 
-const string Setting::START_TIME = "00:00:000";
 const string Setting::END_TIME = "02:15:000";
 const string Setting::TIME_BUY_GUN = "00:45:000";
 const string Setting::TIME_ADD_PLAYER = "00:03:000";
 
 const Gun *Setting::START_GUN = Guns::get_gun("knife", GlobalVariable::access_level::setting);
+
+#include "../src/Setting.h"
 
 #endif //COUNTER_STRIKE_QUERA_SETTING_H
---
+++ src/Setting.h
@@ -0,0 +1,21 @@
+//
+// Created by moosavi on 10/22/22.
+//
+
+auto Setting::get_start_money() { return Setting::START_MONEY; }
+
+auto Setting::get_max_money() { return Setting::MAX_MONEY; }
+
+auto Setting::get_won_money() { return Setting::WON_MONEY; }
+
+auto Setting::get_lose_money() { return Setting::LOSE_MONEY; }
+
+auto Setting::get_end_time() { return Setting::END_TIME; }
+
+auto Setting::get_time_buy_gun() { return Setting::TIME_BUY_GUN; }
+
+auto Setting::get_time_add_player() { return Setting::TIME_ADD_PLAYER; }
+
+auto Setting::get_start_gun() { return Setting::START_GUN; }
+
+auto Setting::get_max_member_team() { return Setting::MAX_MEMBER_TEAM; }
