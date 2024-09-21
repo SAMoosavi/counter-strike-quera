221fff35d2638a6bd798191f262a52179c1d5bad
SAMoosavi <moosavi238@gmail.com>
2024-09-21 17:01:26 +0330

    fix test :/

--- rust/src/setting.rs
+++ rust/src/setting.rs
@@ -173,24 +173,28 @@ mod tests_setting {
     use std::sync::Arc;
     #[test]
     pub fn test_get_and_set_max_money_of_player() {
+        Setting::reset();
         assert!(Setting::set_max_money_of_player(1000).is_ok());
         assert_eq!(Setting::get_max_money_of_player(), 1000);
         Setting::reset();
     }
     #[test]
     pub fn test_set_max_money_of_player_fail() {
+        Setting::reset();
         assert!(Setting::set_max_money_of_player(0).is_err());
         assert_eq!(Setting::get_max_money_of_player(), 0);
         Setting::reset();
     }
     #[test]
     pub fn test_get_and_set_default_money_of_player() {
+        Setting::reset();
         assert!(Setting::set_default_money_of_player(100).is_ok());
         assert_eq!(Setting::get_default_money_of_player(), 100);
         Setting::reset();
     }
     #[test]
     pub fn test_set_default_money_of_player_fail() {
+        Setting::reset();
         assert!(Setting::set_default_money_of_player(0).is_err());
         assert_eq!(Setting::get_default_money_of_player(), 0);
         Setting::reset();
@@ -198,6 +202,7 @@ mod tests_setting {
     #[test]
     pub fn test_get_and_set_default_gun() {
         let gun = Arc::new(Gun::new("knife".to_string(), 100, 20, 100, TypeOfGun::Knife));
+        Setting::reset();
         assert!(Setting::set_default_gun(gun.clone()).is_ok());
         assert_eq!(Setting::get_default_gun(), Some(gun));
         Setting::reset();
@@ -205,12 +210,14 @@ mod tests_setting {
     #[test]
     pub fn test_set_default_gun_fail() {
         let gun = Arc::new(Gun::new("not knife".to_string(), 100, 20, 100, TypeOfGun::Pistol));
+        Setting::reset();
         assert!(Setting::set_default_gun(gun).is_err());
         assert_eq!(Setting::get_default_gun(), None);
         Setting::reset();
     }
     #[test]
     pub fn test_get_setting() {
+        Setting::reset();
         let setting = Setting::get_setting();
         assert_eq!(setting.lock().unwrap().max_money_of_player, 0);
         assert_eq!(setting.lock().unwrap().default_money_of_player, 0);
@@ -219,6 +226,7 @@ mod tests_setting {
     }
     #[test]
     pub fn test_setting_display() {
+        Setting::reset();
         let setting = Setting::get_setting();
         assert_eq!(format!("{}", setting.lock().unwrap()), "Setting {max_money_of_player: 0, default_money_of_player: 0, default_gun: None}");
         Setting::reset();
