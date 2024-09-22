52f8360cbcd00dae7e7b997c965d3bc90ccf6232
SAMoosavi <moosavi238@gmail.com>
2024-09-22 16:04:50 +0330

    implement add_player and fix test bugs

--- rust/src/team.rs
+++ rust/src/team.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use crate::game_time::GameTime;
 use crate::gun::Guns;
 use crate::player::Player;
+use crate::setting::Setting;
 
 pub struct Team {
     players: Vec<Arc<Player>>,
@@ -17,18 +18,36 @@ impl Team {
     }
 
     pub fn add_player(&mut self, name: &str, time: &GameTime) -> Result<(), String> {
-        Ok(())
+        let max_money_of_player = Setting::get_max_money_of_player();
+        if max_money_of_player == 0 { 
+            return Err("the maximum number of players doesn't set!".to_string());
+        }
+        if (max_money_of_player as usize) == self.players.len() {
+            return Err("the team is full!".to_string());
+        }
+        
+        let player = Player::new(name.to_string(), time.clone())?;
+        Ok(self.players.push(Arc::new(player)))
     }
 }
 
 
 #[cfg(test)]
 mod tests {
+    use std::sync::Arc;
+
     use crate::game_time::GameTime;
-    use crate::gun::Guns;
+    use crate::gun::{Guns, Gun, TypeOfGun};
     use crate::player::Player;
     use crate::setting::Setting;
     use super::Team;
+
+    fn fill_setting_for_create_player(){
+        let gun = Gun::new("knife".to_string(), 100, 10, 20, TypeOfGun::Knife);
+        Setting::set_default_gun(Arc::new(gun)).unwrap();
+        Setting::set_default_money_of_player(1000).unwrap();
+    }
+
     #[test]
     pub fn test_default() {
         let team = Team::default();
@@ -39,6 +58,7 @@ mod tests {
     #[test]
     pub fn test_add_player_should_be_return_error_when_does_not_set_max_number_of_players_in_setting() {
         Setting::reset();
+        fill_setting_for_create_player();
         let mut team = Team::default();
         let time = GameTime::new(0, 0, 0, 1);
 
@@ -52,6 +72,7 @@ mod tests {
     #[test]
     pub fn test_add_player_should_be_return_error_when_team_is_full() {
         Setting::reset();
+        fill_setting_for_create_player();
         let mut team = Team::default();
         let time = GameTime::new(0, 0, 0, 1);
         Setting::set_max_money_of_player(1).unwrap();
@@ -67,6 +88,7 @@ mod tests {
     #[test]
     pub fn test_add_player_should_be_return_error_when_player_exists_with_same_name() {
         Setting::reset();
+        fill_setting_for_create_player();
         let mut team = Team::default();
         let time = GameTime::new(0, 0, 0, 1);
         let name = "Player";
@@ -83,6 +105,7 @@ mod tests {
     #[test]
     pub fn test_add_player_added_to_least_of_players() {
         Setting::reset();
+        fill_setting_for_create_player();
         let mut team = Team::default();
         Setting::set_max_money_of_player(2).unwrap();
         let name = "Player";
