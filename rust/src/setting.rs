b12cf31b7c94d1487a421ba5ade306b0bce651bd
SAMoosavi <moosavi238@gmail.com>
2024-09-22 20:08:37 +0330

    simplify

--- rust/src/setting.rs
+++ rust/src/setting.rs
@@ -1,6 +1,8 @@
 use crate::gun::{Gun, TypeOfGun};
-use std::sync::{Arc, Mutex, OnceLock};
-use std::fmt;
+use std::{
+    fmt,
+    sync::{Arc, Mutex, OnceLock},
+};
 
 #[derive(Debug)]
 struct SettingData {
@@ -8,17 +10,21 @@ struct SettingData {
     default_money_of_player: u32,
     default_gun: Option<Arc<Gun>>,
     max_number_of_team_players: u32,
+    won_team_money: u32,
+    lose_team_money: u32,
 }
 
 impl fmt::Display for SettingData {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "Setting {{max_money_of_player: {}, default_money_of_player: {}, default_gun: {:?}, max_number_of_team_players: {}}}",
+            "Setting {{max_money_of_player: {}, default_money_of_player: {}, default_gun: {:?}, max_number_of_team_players: {}, won_team_money: {}, lose_team_money: {}}}",
             self.max_money_of_player,
             self.default_money_of_player,
             self.default_gun,
-            self.max_number_of_team_players
+            self.max_number_of_team_players,
+            self.won_team_money,
+            self.lose_team_money
         )
     }
 }
@@ -30,105 +36,19 @@ impl SettingData {
             default_money_of_player: 0,
             default_gun: None,
             max_number_of_team_players: 0,
+            won_team_money: 0,
+            lose_team_money: 0,
         }
     }
 
-
-    #[allow(dead_code)]
-    pub fn set_max_money_of_player(&mut self, max_money_of_player: u32) -> Result<(), String> {
-        if max_money_of_player <= 0 {
-            return Err("the max money of player should be greater than 0.".to_string());
-        }
-        self.max_money_of_player = max_money_of_player;
-        Ok(())
-    }
-
-    #[allow(dead_code)]
-    pub fn set_default_money_of_player(&mut self, default_money_of_player: u32) -> Result<(), String> {
-        if default_money_of_player <= 0 {
-            return Err("the default money of player should be greater than 0.".to_string());
-        }
-        self.default_money_of_player = default_money_of_player;
-        Ok(())
-    }
-
-    #[allow(dead_code)]
-    pub fn set_default_gun(&mut self, gun: Arc<Gun>) -> Result<(), String> {
-        if gun.get_type_of() != TypeOfGun::Knife {
-            return Err("the default gun should be knife type.".to_string());
-        }
-        self.default_gun = Some(gun);
-        Ok(())
-    }
-
-    #[allow(dead_code)]
-    pub fn set_max_number_of_team_players(&mut self, max_number_of_team_players: u32) -> Result<(), String> {
-        if max_number_of_team_players == 0 {
-            return Err("the max_number_of_team_players should be positive!".to_string());
-        }
-        Ok(self.max_number_of_team_players = max_number_of_team_players)
-    }
-    #[allow(dead_code)]
-    pub fn get_max_number_of_team_players(&self) -> u32 {
-        self.max_number_of_team_players
-    }
     #[cfg(test)]
     pub fn reset(&mut self) {
         self.max_money_of_player = 0;
         self.default_money_of_player = 0;
         self.default_gun = None;
-    }
-}
-
-#[cfg(test)]
-mod tests_setting_data {
-    use std::sync::Arc;
-    use super::SettingData;
-    use crate::gun::{Gun, TypeOfGun};
-    #[test]
-    pub fn test_default_setting_data() {
-        let setting = SettingData::default();
-        assert_eq!(setting.max_money_of_player, 0);
-        assert_eq!(setting.default_money_of_player, 0);
-        assert_eq!(setting.default_gun, None);
-    }
-    #[test]
-    pub fn test_set_max_money_of_player() {
-        let mut setting = SettingData::default();
-        assert!(setting.set_max_money_of_player(1000).is_ok());
-        assert_eq!(setting.max_money_of_player, 1000);
-    }
-    #[test]
-    pub fn test_set_max_money_of_player_fail_when_the_money_is_zero() {
-        let mut setting = SettingData::default();
-        assert!(setting.set_max_money_of_player(0).is_err());
-        assert_eq!(setting.max_money_of_player, 0);
-    }
-    #[test]
-    pub fn test_set_default_money_of_player() {
-        let mut setting = SettingData::default();
-        assert!(setting.set_default_money_of_player(100).is_ok());
-        assert_eq!(setting.default_money_of_player, 100);
-    }
-    #[test]
-    pub fn test_set_default_money_of_player_fail() {
-        let mut setting = SettingData::default();
-        assert!(setting.set_default_money_of_player(0).is_err());
-        assert_eq!(setting.default_money_of_player, 0);
-    }
-    #[test]
-    pub fn test_set_default_gun() {
-        let gun = Arc::new(Gun::new("knife".to_string(), 100, 20, 100, TypeOfGun::Knife));
-        let mut setting = SettingData::default();
-        assert!(setting.set_default_gun(gun.clone()).is_ok());
-        assert_eq!(setting.default_gun, Some(gun));
-    }
-    #[test]
-    pub fn test_set_default_gun_fail() {
-        let gun = Arc::new(Gun::new("not knife".to_string(), 100, 20, 100, TypeOfGun::Pistol));
-        let mut setting = SettingData::default();
-        assert!(setting.set_default_gun(gun).is_err());
-        assert_eq!(setting.default_gun, None);
+        self.max_number_of_team_players = 0;
+        self.won_team_money = 0;
+        self.lose_team_money = 0;
     }
 }
 
@@ -146,17 +66,23 @@ impl Setting {
 
     #[allow(dead_code)]
     pub fn set_max_money_of_player(max_money_of_player: u32) -> Result<(), String> {
-        let mut setting = Self::get_setting().lock().unwrap();
-        setting.set_max_money_of_player(max_money_of_player)
+        if max_money_of_player <= 0 {
+            return Err("the max money of player should be greater than 0.".to_string());
+        }
+        Ok(Self::get_setting().lock().unwrap().max_money_of_player = max_money_of_player)
     }
+
     #[allow(dead_code)]
     pub fn get_default_money_of_player() -> u32 {
         Self::get_setting().lock().unwrap().default_money_of_player
     }
     #[allow(dead_code)]
     pub fn set_default_money_of_player(default_money_of_player: u32) -> Result<(), String> {
-        let mut setting = Self::get_setting().lock().unwrap();
-        setting.set_default_money_of_player(default_money_of_player)
+        if default_money_of_player <= 0 {
+            return Err("the default money of player should be greater than 0.".to_string());
+        }
+
+        Ok(Self::get_setting().lock().unwrap().default_money_of_player = default_money_of_player)
     }
     pub fn get_default_gun() -> Option<Arc<Gun>> {
         Self::get_setting().lock().unwrap().default_gun.clone()
@@ -164,20 +90,56 @@ impl Setting {
 
     #[allow(dead_code)]
     pub fn set_default_gun(gun: Arc<Gun>) -> Result<(), String> {
-        let mut setting = Self::get_setting().lock().unwrap();
-        setting.set_default_gun(gun)
+        if gun.get_type_of() != TypeOfGun::Knife {
+            return Err("the default gun should be knife type.".to_string());
+        }
+
+        Ok(Self::get_setting().lock().unwrap().default_gun = Some(gun))
     }
 
     #[allow(dead_code)]
     pub fn set_max_number_of_team_players(max_number_of_team_players: u32) -> Result<(), String> {
-        let mut setting = Self::get_setting().lock().unwrap();
-        setting.set_max_number_of_team_players(max_number_of_team_players)
+        if max_number_of_team_players == 0 {
+            return Err("the max_number_of_team_players should be positive!".to_string());
+        }
+        Ok(Self::get_setting()
+            .lock()
+            .unwrap()
+            .max_number_of_team_players = max_number_of_team_players)
     }
 
     #[allow(dead_code)]
     pub fn get_max_number_of_team_players() -> u32 {
-        let setting = Self::get_setting().lock().unwrap();
-        setting.get_max_number_of_team_players()
+        Self::get_setting()
+            .lock()
+            .unwrap()
+            .max_number_of_team_players
+    }
+
+    #[allow(dead_code)]
+    pub fn set_won_team_money(won_team_money: u32) -> Result<(), String> {
+        if won_team_money == 0 {
+            return Err("the won_team_money should be positive!".to_string());
+        }
+        Ok(Self::get_setting().lock().unwrap().won_team_money = won_team_money)
+    }
+
+    #[allow(dead_code)]
+    pub fn get_won_team_money() -> u32 {
+        Self::get_setting().lock().unwrap().won_team_money
+    }
+
+    #[allow(dead_code)]
+    pub fn set_lose_team_money(lose_team_money: u32) -> Result<(), String> {
+        if lose_team_money == 0 {
+            return Err("the lose_team_money should be positive!".to_string());
+        }
+        Ok(Self::get_setting().lock().unwrap().lose_team_money = lose_team_money)
+    }
+
+    #[allow(dead_code)]
+    pub fn get_lose_team_money() -> u32 {
+        Self::get_setting().lock().unwrap().lose_team_money
     }
 
     #[cfg(test)]
@@ -227,7 +189,13 @@ mod tests_setting {
     }
     #[test]
     pub fn test_get_and_set_default_gun() {
-        let gun = Arc::new(Gun::new("knife".to_string(), 100, 20, 100, TypeOfGun::Knife));
+        let gun = Arc::new(Gun::new(
+            "knife".to_string(),
+            100,
+            20,
+            100,
+            TypeOfGun::Knife,
+        ));
         Setting::reset();
         assert!(Setting::set_default_gun(gun.clone()).is_ok());
         assert_eq!(Setting::get_default_gun(), Some(gun));
@@ -235,7 +203,13 @@ mod tests_setting {
     }
     #[test]
     pub fn test_set_default_gun_fail() {
-        let gun = Arc::new(Gun::new("not knife".to_string(), 100, 20, 100, TypeOfGun::Pistol));
+        let gun = Arc::new(Gun::new(
+            "not knife".to_string(),
+            100,
+            20,
+            100,
+            TypeOfGun::Pistol,
+        ));
         Setting::reset();
         assert!(Setting::set_default_gun(gun).is_err());
         assert_eq!(Setting::get_default_gun(), None);
