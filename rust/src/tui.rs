90d47e9ba2bfd05ff66915ecc37b7757de6a0fa5
SAMoosavi <moosavi238@gmail.com>
2024-10-27 17:15:33 +0330

    move CommandNoneHandler to a file

--- rust/src/tui.rs
+++ rust/src/tui.rs
@@ -1,20 +1,20 @@
+mod command_none_handler;
+
 use std::io;
 
 use ratatui::{
-    crossterm::event::{self, Event, KeyCode, KeyEventKind},
+    crossterm::event::{self, Event},
     layout::Rect,
     prelude::{Constraint, Direction, Layout},
-    style::{Color, Style, Stylize},
-    text::{Line, Span},
+    style::{Style, Stylize},
+    text::Line,
     widgets::{Block, BorderType, Borders, List, ListItem},
     DefaultTerminal, Frame,
 };
-use regex::Regex;
+use command_none_handler::CommandNoneHandler;
 
 use crate::{
-    game::{Game, TeamId},
-    game_time::GameTime,
-    gun::TypeOfGun,
+    game::{Game},
 };
 
 enum GameEvent {
@@ -29,138 +29,6 @@ trait GameCommandHandler {
     fn event_handler(&mut self, event: Event) -> GameEvent;
 }
 
-struct GameCommandNone {
-    selected: usize,
-    commands: Vec<&'static str>,
-    commands_filtered: Vec<&'static str>,
-    search: String,
-}
-
-impl GameCommandNone {
-    pub fn default() -> Self {
-        Self {
-            selected: 0,
-            commands: GameCommand::get_commands_name(),
-            commands_filtered: GameCommand::get_commands_name(),
-            search: "".to_string(),
-        }
-    }
-
-    fn filter_commands(&mut self) {
-        let search = Regex::new(&self.search).unwrap();
-
-        self.commands_filtered = self
-            .commands
-            .iter()
-            .filter(|x| search.is_match(x))
-            .cloned()
-            .collect();
-    }
-
-    fn reset_selected(&mut self) {
-        self.selected = 0;
-    }
-
-    fn select_next(&mut self) {
-        self.selected = if !self.commands_filtered.is_empty()
-            && self.commands_filtered.len() - 1 != self.selected
-        {
-            self.selected + 1
-        } else {
-            self.selected
-        };
-    }
-
-    fn select_prev(&mut self) {
-        self.selected = if self.selected != 0 {
-            self.selected - 1
-        } else {
-            0
-        };
-    }
-
-    fn select_first(&mut self) {
-        self.selected = 0;
-    }
-
-    fn select_last(&mut self) {
-        self.selected = if self.commands_filtered.is_empty() {
-            0
-        } else {
-            self.commands_filtered.len() - 1
-        };
-    }
-
-    fn push_search(&mut self, c: char) {
-        self.reset_selected();
-        self.search.push(c);
-        self.filter_commands();
-    }
-
-    fn pop_search(&mut self) {
-        if !self.search.is_empty() {
-            self.reset_selected();
-            self.search.pop();
-            self.filter_commands();
-        }
-    }
-}
-impl GameCommandHandler for GameCommandNone {
-    fn run(&mut self, frame: &mut Frame, rect: Rect) {
-        let mut lines: Vec<ListItem> = vec![];
-
-        for (index, command) in self.commands_filtered.iter().enumerate() {
-            lines.push(ListItem::new(Line::from(*command).style(
-                if self.selected != index {
-                    Style::new().yellow()
-                } else {
-                    Style::new().black().bg(Color::Yellow)
-                },
-            )));
-        }
-
-        let list = List::new(lines).block(
-            Block::default()
-                .borders(Borders::ALL)
-                .title(Line::from(" Game Command ").right_aligned())
-                .title(
-                    Line::from_iter([
-                        Span::from(" Search Command: "),
-                        Span::from(&self.search).fg(Color::Red),
-                        Span::from(" "),
-                    ])
-                        .centered(),
-                )
-                .border_type(BorderType::Rounded),
-        );
-        frame.render_widget(list, rect);
-    }
-
-    fn event_handler(&mut self, event: Event) -> GameEvent {
-        match event {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                match key_event.code {
-                    KeyCode::Backspace => self.pop_search(),
-                    KeyCode::Enter => {
-                        return GameEvent::ChangeState(
-                            self.commands_filtered[self.selected].to_string(),
-                        )
-                    }
-                    KeyCode::Up => self.select_prev(),
-                    KeyCode::Down => self.select_next(),
-                    KeyCode::Home => self.select_first(),
-                    KeyCode::End => self.select_last(),
-                    KeyCode::Delete => self.pop_search(),
-                    KeyCode::Char(x) => self.push_search(x),
-                    KeyCode::Null | KeyCode::Esc => return GameEvent::Back,
-                    _ => {}
-                };
-            }
-            _ => {}
-        };
-        GameEvent::None
-    }
-}
 
 enum GameCommand {
     AddUser,
@@ -169,7 +37,7 @@ enum GameCommand {
     Tap,
     Buy,
     ScoreBoard,
-    None(GameCommandNone),
+    None(CommandNoneHandler),
 }
 
 impl GameCommand {
@@ -197,7 +65,7 @@ enum Log {
 }
 
 struct App<'a> {
-    game: &'a mut Game,
+    _game: &'a mut Game,
     exit: bool,
     logs: Vec<Log>,
     state: GameCommand,
@@ -206,10 +74,10 @@ struct App<'a> {
 impl<'a> App<'a> {
     pub fn new(game: &'a mut Game) -> Self {
         Self {
-            game,
+            _game: game,
             exit: false,
             logs: vec![],
-            state: GameCommand::None(GameCommandNone::default()),
+            state: GameCommand::None(CommandNoneHandler::default()),
         }
     }
 
@@ -271,7 +139,7 @@ impl<'a> App<'a> {
 
     fn game_event_handler(&mut self, event: GameEvent) {
         match event {
-            GameEvent::Back => { self.state = GameCommand::None(GameCommandNone::default()); }
+            GameEvent::Back => { self.state = GameCommand::None(CommandNoneHandler::default()); }
             GameEvent::ChangeState(state) => self.state = match &state[..] {
                 "add-user" => GameCommand::AddUser,
                 "get-money" => GameCommand::GetMoney,
@@ -279,7 +147,7 @@ impl<'a> App<'a> {
                 "tap" => GameCommand::Tap,
                 "buy" => GameCommand::Buy,
                 "score-board" => GameCommand::ScoreBoard,
-                "none" => GameCommand::None(GameCommandNone::default()),
+                "none" => GameCommand::None(CommandNoneHandler::default()),
                 _ => panic!("Invalid state: {}", state),
             },
             GameEvent::None => {}
